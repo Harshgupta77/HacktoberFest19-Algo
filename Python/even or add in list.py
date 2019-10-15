0e2c4e015538a165b93ffcca6c3823486a529ca7
Harshgupta77 <48287995+Harshgupta77@users.noreply.github.com>
2019-10-15 18:49:13 +0530

    Create even or add in list.py

---
+++ Python/even or add in list.py
@@ -0,0 +1,14 @@
+list = [10, 21, 4, 45, 66, 93, 1] 
+
+even_num , odd_num = 0, 0
+ 
+for num in list1: 
+	 
+	if num % 2 == 0: 
+		even_num += 1
+
+	else: 
+		odd_num += 1
+		
+print("Even numbers in the list: ", even_num) 
+print("Odd numbers in the list: ", odd_num) 
